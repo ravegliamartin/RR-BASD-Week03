fb16320a679ea8d6e3a7b03a1a9c5860d1385f5e
Martin Raveglia <ravegliamartin@gmail.com>
2021-10-14 19:23:25 -0300

    scripts file 'main.js' added, linked to html and all form fields linked

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const fullName = document.querySelector('#fname')
+const mail = document.querySelector('#mail')
+const pass = document.querySelector('#pass')
+const repeatPass = document.querySelector('#repeat-pass')
+const age = document.querySelector('#age')
+const phone = document.querySelector('#phone')
+const address = document.querySelector('#address')
+const city = document.querySelector('#city')
+const zip = document.querySelector('#zip')
+const id = document.querySelector('#id')
+
+const main = () => {
+
+}
+
+window.addEventListener('load', main)
